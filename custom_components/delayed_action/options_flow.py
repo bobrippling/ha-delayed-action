4948e7e3e6f9bebf3fdcccc2376f55480513d130
Rob Pilling <robpilling@gmail.com>
2025-05-29 12:02:31 +0100

    Don't explicitly set `config_entry`
    See https://github.com/bhuebschen/delayed_action/issues/5

--- custom_components/delayed_action/options_flow.py
+++ custom_components/delayed_action/options_flow.py
@@ -9,7 +9,7 @@ _LOGGER = logging.getLogger(__name__)
 
 class DelayedActionOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
-        self.config_entry = config_entry
+        pass
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
